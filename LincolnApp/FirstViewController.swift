f7bb7a3c475a551e1cf79e35b6759ff0b2823885
Thomas Shealy <thomasshealy@jillians-macbook-pro.local>
2017-06-08 15:46:58 -0400

    Initial Commit

---
+++ LincolnApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LincolnApp
+//
+//  Created by Thomas Shealy on 6/8/17.
+//  Copyright © 2017 Thomas Shealy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
